c9ff76bf5bf95d4a1216cd04d86f796033b2bccc
zzoman <reumia@gmail.com>
2017-06-02 18:11:02 +0900

    Bind Attribute to Input

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="app">
+        <h1>{{ msg }}</h1>
+        <input-component name="poi" id="01"></input-component>
+        <input-component name="poi" id="02"></input-component>
+        <input-component name="poi" id="03"></input-component>
+        <input-component name="poi" id="04"></input-component>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    import InputComponent from './components/InputComponent.vue';
+
+    export default {
+        name: 'app',
+        components: {
+            'input-component': InputComponent
+        },
+        data () {
+            return {
+                msg: 'Equal Encounter'
+            }
+        }
     }
-  }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    html,
+    body {
+        padding: 0;
+        margin: 0;
+    }
 
-h1, h2 {
-  font-weight: normal;
-}
+    .app {
+        font-family: sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #333;
+        text-align: center;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/components/InputComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <label>{{name}}_{{id}}</label>
+        <input type="text" name="" placeholder="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'input-component',
+        props: ['id', 'name'],
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
